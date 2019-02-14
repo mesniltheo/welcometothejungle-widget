c78bfd2e2ebdc33e6e1a7ff1db3ac4c989360339
Theo Mesnil <mesniltheo@gmail.com>
2019-02-14 18:59:26 +0100

    add styled component 💅

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const TestingStyledComponent = styled.h1`
+  color: #00c29a;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <TestingStyledComponent>let's go !</TestingStyledComponent>
       </div>
     );
   }
