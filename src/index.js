94ce0a56ced7647cbb4a8c2b53f65a9bea57dd09
Theo Mesnil <mesniltheo@gmail.com>
2019-02-14 19:05:13 +0100

    add normalize to reset dom css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
+import { Normalize } from "styled-normalize";
 import React from "react";
 import ReactDOM from "react-dom";
+
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// add styled-normalize to reset dom css
+ReactDOM.render(
+  <React.Fragment>
+    <Normalize />
+    <App />
+  </React.Fragment>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
