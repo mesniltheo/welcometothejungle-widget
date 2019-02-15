49ed3c614ccdfa6889616a2bfa39886c40a38446
Theo Mesnil <mesniltheo@gmail.com>
2019-02-15 15:42:49 +0100

    chore: add config for widget

---
+++ src/config.js
@@ -0,0 +1,71 @@
+import logoCompany from "./assets/logo-wttj.png";
+
+export const config = {
+  company: {
+    name: "Welcome to the jungle",
+    url: "wttj",
+    logo: logoCompany
+  },
+  content: [
+    {
+      type: "video",
+      title: "Bertrand Uzeel",
+      subtitle: "Co-Founder",
+      caption:
+        "https://cdn.welcometothejungle.co/uploads/video/image/1480/151603/small_wttj_video_0679859b-6e84-4549-bbc4-1330eddcac57.jpg",
+      embed: "1gPC5eOJy8I"
+    },
+    {
+      type: "picture",
+      caption:
+        "https://cdn.welcometothejungle.co/uploads/image/file/7269/151601/small_wttj-annonces_583853d2-7b7d-4827-beb6-005c9c814df7.jpg",
+      url:
+        "https://cdn.welcometothejungle.co/uploads/image/file/7269/151601/large_wttj-annonces_583853d2-7b7d-4827-beb6-005c9c814df7.jpg"
+    },
+    {
+      type: "picture",
+      caption:
+        "https://cdn.welcometothejungle.co/uploads/image/file/7220/151601/small_wttj-annonces_fde76898-d6a4-4540-af78-f26ec06ba201.jpg",
+      url:
+        "https://cdn.welcometothejungle.co/uploads/image/file/7220/151601/large_wttj-annonces_fde76898-d6a4-4540-af78-f26ec06ba201.jpg"
+    },
+    {
+      type: "quote",
+      text: "Life is short, work somewhere awesome."
+    },
+    {
+      type: "video",
+      title: "Kevin Le Roy",
+      subtitle: "CTO",
+      caption:
+        "https://cdn.welcometothejungle.co/uploads/video/image/1540/151603/small_wttj_video_267f7b56-911e-4888-9721-085e83f9f479.jpg",
+      embed: "z09vIvjTw_Y"
+    },
+    {
+      type: "video",
+      title: "Mélanie Babinot",
+      subtitle: "Sales Development Manager",
+      caption:
+        "https://cdn.welcometothejungle.co/uploads/video/image/9401/151602/small_wttj_video_3bbaeeb9-2f2f-4a1b-ac14-e6be495d3bb4.jpg",
+      embed: "vNSJY2iT5I"
+    },
+    {
+      type: "picture",
+      caption:
+        "https://cdn.welcometothejungle.co/uploads/image/file/7242/151601/small_wttj-inside_06571833-82ab-42e7-8fe2-0f004ecf79c8.jpg",
+      url:
+        "https://cdn.welcometothejungle.co/uploads/image/file/7242/151601/large_wttj-inside_06571833-82ab-42e7-8fe2-0f004ecf79c8.jpg"
+    },
+    {
+      type: "picture",
+      caption:
+        "https://cdn.welcometothejungle.co/uploads/image/file/7252/151601/small_wttj-jobs_b3bbd3a7-0c09-403a-a446-9f29d0b09c3f.jpg",
+      url:
+        "https://cdn.welcometothejungle.co/uploads/image/file/7252/151601/large_wttj-jobs_b3bbd3a7-0c09-403a-a446-9f29d0b09c3f.jpg"
+    },
+    {
+      type: "quote",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    }
+  ]
+};
